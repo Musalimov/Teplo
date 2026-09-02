c8032fa5d079dfd44fdffc3a298dafaf1af4eab3
Robert Musalimov <musalimofff@gmail.com>
2026-09-02 20:17:04 +1000

    Fixed some bugs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Версию менять при каждом обновлении файлов игры — старый кэш тогда стирается.
-const CACHE = "teplo-5";
+const CACHE = "teplo-7";
 
 self.addEventListener("install", () => self.skipWaiting());
 
